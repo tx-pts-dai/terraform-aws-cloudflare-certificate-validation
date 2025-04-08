835824747ba67d021ecd15a6211d638ba07de77b
Davidoutz <david.beauverd@tx.group>
2025-04-08 09:19:59 +0200

    init module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -18,7 +18,7 @@ terraform {
 
 module "dns" {
   source                    = "../../"
-  create_validation_records = true # default value
+  create_validation_records = false # default is true
   cloudflare_secret_id      = "dai/cloudflare/apiToken"
   records_map = {
     "foo.examples.tamedia.ch" = {
